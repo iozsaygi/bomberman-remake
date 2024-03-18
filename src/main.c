188b9e31ec97dece3cfa044414c1d480dbb302ea
İsmail Özsaygı <devozsaygi@gmail.com>
2024-03-18 23:00:23 +0300

    Updated main signature

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "debugger.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     debugger_log(TRACE, "This is trace message %d", 0);
     debugger_log(WARNING, "This is warning message %d", 1);
     debugger_log(CRITICAL, "This is critical message %d", 2);
