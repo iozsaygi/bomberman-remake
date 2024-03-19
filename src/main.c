ab4ff94ee205dc0f2612178a7bc736b6f281d41b
İsmail Özsaygı <devozsaygi@gmail.com>
2024-03-19 23:21:47 +0300

    Added dummy code for SDL usage

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include "debugger.h"
+#include <SDL.h>
 
 int main(int argc, char* argv[]) {
-    debugger_log(TRACE, "This is trace message %d", 0);
-    debugger_log(WARNING, "This is warning message %d", 1);
-    debugger_log(CRITICAL, "This is critical message %d", 2);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        debugger_log(CRITICAL, "Failed to initialize SDL with video subsystem, the reason was: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Quit();
     return 0;
 }
