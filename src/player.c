2e0ecc74dea4b180cb21ba9b622aa3e76b19f5d5
İsmail Özsaygı <devozsaygi@gmail.com>
2024-03-30 00:03:28 +0300

    Removed strange multiply operation

--- src/player.c
+++ src/player.c
@@ -7,8 +7,8 @@ struct player_movement player_movement;
 void player_initialize() {
     player_transform.position.x = 120;
     player_transform.position.y = 100;
-    player_transform.scale.x = DEFAULT_ENTITY_SCALE * 1;
-    player_transform.scale.y = DEFAULT_ENTITY_SCALE * 1;
+    player_transform.scale.x = DEFAULT_ENTITY_SCALE;
+    player_transform.scale.y = DEFAULT_ENTITY_SCALE;
     player_movement.speed = 5.0f;
 }
 
