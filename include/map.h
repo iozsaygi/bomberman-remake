55c9e365fa824bb6ec7113f432bef3407ba2f033
İsmail Özsaygı <devozsaygi@gmail.com>
2024-04-05 21:06:23 +0300

    Updated map size

--- include/map.h
+++ include/map.h
@@ -6,9 +6,9 @@
 
 // Game window is 320x240, each node has 16 pixel for height and width.
 // The map is going to fully allocate the entire game window.
-// Map size is -> (320 / 32) x (256 / 32) = (10) x (8) = 80
+// Map size is -> (320 / 32) x (288 / 32) = (10) x (9) = 90
 #define MAP_WIDTH 10
-#define MAP_HEIGHT 8
+#define MAP_HEIGHT 9
 
 extern struct node map_nodes[MAP_WIDTH][MAP_HEIGHT];
 
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Try to create platform context.
-    if (game_createPlatformContext(320, 256, "Bomberman: Remake", 15) == SUCCESS) {
+    if (game_createPlatformContext(320, 288, "Bomberman: Remake", 15) == SUCCESS) {
         scene_initialize();
         // We have the game's platform context here. Start to update scene.
         scene_tick(game_platformContext);
