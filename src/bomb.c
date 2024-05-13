a604e13ef7815da4e6a0c0fa6bd321ba3cd29f89
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-13 21:51:00 +0300

    Fixed compilation errors for builds

--- src/bomb.c
+++ src/bomb.c
@@ -26,10 +26,10 @@ void bomb_explode(struct bomb_transform* bombTransform) {
 void bomb_render(struct game_platformContext gamePlatformContext, struct bomb_transform* bombTransform) {
     SDL_SetRenderDrawColor(gamePlatformContext.renderer, 0, 255, 0, 255);
     SDL_Rect renderRect;
-    renderRect.w = (int) bombTransform.scale.x;
-    renderRect.h = (int) bombTransform.scale.y;
-    renderRect.x = (int) bombTransform.position.x;
-    renderRect.y = (int) bombTransform.position.y;
+    renderRect.w = (int) bombTransform->scale.x;
+    renderRect.h = (int) bombTransform->scale.y;
+    renderRect.x = (int) bombTransform->position.x;
+    renderRect.y = (int) bombTransform->position.y;
 
     SDL_RenderFillRect(gamePlatformContext.renderer, &renderRect);
 
