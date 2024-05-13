f8f84b913f3072f74b7468d5a33503108374fd52
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-13 21:32:20 +0300

    Added progress for bomb explosion

--- include/bomb.h
+++ include/bomb.h
@@ -4,12 +4,15 @@
 #include "game.h"
 #include "vector2.h"
 
+#define BOMB_LIFETIME_IN_SECONDS 3
+
 struct bomb_transform {
     struct vector2 position;
 };
 
-void bomb_createAt(struct vector2 position);
+struct bomb_transform* bomb_createAt(struct vector2 position);
 void bomb_tick(float deltaTime);
+void bomb_explode(struct bomb_transform* bombTransform);
 void bomb_render(struct game_platformContext gamePlatformContext);
 
 #endif// !BOMB_H
--- src/bomb.c
+++ src/bomb.c
@@ -1,10 +1,23 @@
 #include "bomb.h"
+#include <assert.h>
+#include <stdlib.h>
 
-void bomb_createAt(struct vector2 position) {
+struct bomb_transform* bomb_createAt(struct vector2 position) {
     // Try to allocate bomb first.
+    struct bomb_transform* bombTransform = (struct bomb_transform*) malloc(sizeof(struct bomb_transform));
+    assert(bombTransform != NULL);
+    bombTransform->position = position;
+
+    return bombTransform;
 }
 
 void bomb_tick(float deltaTime) {
+}
+
+void bomb_explode(struct bomb_transform* bombTransform) {
+    assert(bombTransform != NULL);
+    // TODO: Apply damage to adjacent nodes.
+    free(bombTransform);
 }
 
 void bomb_render(struct game_platformContext gamePlatformContext) {
