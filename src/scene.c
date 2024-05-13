22d8d5846f32502cd74c75b3a110333afd327f63
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-13 22:18:14 +0300

    Fix attempt for bomb placement logs

--- src/scene.c
+++ src/scene.c
@@ -22,7 +22,7 @@ void scene_requestBombAt(struct vector2 position) {
     }
 
     scene_lastPlantedBomb = bomb_createAt(position);
-    debugger_log(TRACE, "Placed a new bomb at (%d, %d)", position.x, position.y);
+    debugger_log(TRACE, "Placed a new bomb at (%f, %f)", position.x, position.y);
 }
 
 void scene_tick(struct game_platformContext gamePlatformContext) {
