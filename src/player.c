818be2ece2a5ed790394a671c20901f7f4669c4a
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-17 20:34:03 +0300

    SDL render position fix

--- src/player.c
+++ src/player.c
@@ -162,7 +162,7 @@ void player_render(struct game_platformContext gamePlatformContext, struct asset
     renderTransformRect.y = (int) player_transform.position.y;
 
     SDL_Rect texture_rect;
-    texture_rect.x = 0; //the x coordinate
+    texture_rect.x = 70; //the x coordinate
     texture_rect.y = 0; //the y coordinate
     texture_rect.w = 50; //the width of the texture
     texture_rect.h = 50; //the height of the texture
