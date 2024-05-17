87fbc8d187c78626db25d441e4f62039428fbf54
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-17 19:26:44 +0300

    Fix for asset caching

--- src/main.c
+++ src/main.c
@@ -8,18 +8,17 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // Try to load assets before entering the scene context.
-    // Do not launch the game if asset operations are failed.
-    if (assetManager_loadTextures(game_platformContext) == ASSETS_FAILED_TO_LOAD) {
-        scanf("%d");
-        return -1;
-    }
-
     // Try to create platform context.
     if (game_createPlatformContext(352, 288, "Bomberman: Remake", 15) == SUCCESS) {
-        scene_initialize();
-        // We have the game's platform context here. Start to update scene.
-        scene_tick(game_platformContext);
+        // Try to load assets before entering the scene context.
+        // Do not launch the scene if asset operations are failed.
+        if (assetManager_loadTextures(game_platformContext) == ASSETS_LOADED) {
+            scene_initialize();
+            // We have the game's platform context here. Start to update scene.
+            scene_tick(game_platformContext);
+        } else {
+            scanf("%d");
+        }
 
         // Clear the platform context after the main loop, we are done with it.
         game_clearPlatformContext();
