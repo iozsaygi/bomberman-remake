301dad00a1799123252974411397fbe6220cd87c
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-17 19:05:00 +0300

    Remove example code

--- src/asset_manager.c
+++ src/asset_manager.c
@@ -1,30 +1,7 @@
 #include "asset_manager.h"
 #include "debugger.h"
 
-#include <SDL.h>
-#include <stdio.h>
-
 struct assetManager_textures assetManager_textures;
-
-// Function to load a BMP file from a specified path
-SDL_Surface* loadBMP(const char* filePath) {
-    // Initialize SDL video subsystem
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return NULL;
-    }
-
-    // Load BMP image
-    SDL_Surface* loadedSurface = SDL_LoadBMP(filePath);
-    if (loadedSurface == NULL) {
-        printf("Unable to load BMP file %s! SDL_Error: %s\n", filePath, SDL_GetError());
-    }
-
-    // Quit SDL subsystems
-    SDL_Quit();
-
-    return loadedSurface;
-}
 
 enum assetManager_assetOperationResult assetManager_loadTextures() {
     assetManager_textures.textureAtlas = SDL_LoadBMP(TEXTURE_ATLAS_PATH);
