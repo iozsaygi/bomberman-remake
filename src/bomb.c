f6e31970de0e5dd885e3d072331ac043307e3dda
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-17 21:31:39 +0300

    Fixed bomb scale

--- src/bomb.c
+++ src/bomb.c
@@ -9,8 +9,8 @@ struct bomb_transform* bomb_createAt(struct vector2 position) {
     struct bomb_transform* bombTransform = (struct bomb_transform*) malloc(sizeof(struct bomb_transform));
     assert(bombTransform != NULL);
     bombTransform->position = position;
-    bombTransform->scale.x = DEFAULT_ENTITY_SCALE / 2;
-    bombTransform->scale.y = DEFAULT_ENTITY_SCALE / 2;
+    bombTransform->scale.x = DEFAULT_ENTITY_SCALE;
+    bombTransform->scale.y = DEFAULT_ENTITY_SCALE;
     bombTransform->timer = 0.0f;
     bombTransform->isExploded = 1;
 
@@ -55,8 +55,8 @@ void bomb_render(struct game_platformContext gamePlatformContext, struct bomb_tr
     SDL_SetRenderDrawColor(gamePlatformContext.renderer, 255, 0, 0, 255);
 
     SDL_Rect renderTransformRect;
-    renderTransformRect.w = (int) bombTransform->scale.x * 2;
-    renderTransformRect.h = (int) bombTransform->scale.y * 2;
+    renderTransformRect.w = (int) bombTransform->scale.x;
+    renderTransformRect.h = (int) bombTransform->scale.y;
     renderTransformRect.x = (int) bombTransform->position.x;
     renderTransformRect.y = (int) bombTransform->position.y;
 
