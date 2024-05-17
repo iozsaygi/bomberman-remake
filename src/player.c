91f9e0875255004511d9764e18b4a1d51c5cd743
İsmail Özsaygı <devozsaygi@gmail.com>
2024-05-17 20:47:11 +0300

    Fix try for texture coordinate

--- src/player.c
+++ src/player.c
@@ -162,7 +162,7 @@ void player_render(struct game_platformContext gamePlatformContext, struct asset
     renderTransformRect.y = (int) player_transform.position.y;
 
     SDL_Rect texture_rect;
-    texture_rect.x = 60; //the x coordinate
+    texture_rect.x = 50; //the x coordinate
     texture_rect.y = 0; //the y coordinate
     texture_rect.w = 10; //the width of the texture
     texture_rect.h = 10; //the height of the texture
